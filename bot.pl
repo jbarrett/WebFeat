6fa380e2321fb3882e0d344556eb7d03ab8d75b3
John Barrett <john@jbrt.org>
2014-11-19 12:00:56 +0000

    Bot app with DuckCoHelp, Seen, Title and Karma loaded.

---
+++ bot.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use Bot::BasicBot::Pluggable;
+
+my $bot = Bot::BasicBot::Pluggable->new(
+    channels => ["#duckduckgo-test"],
+    server   => "irc.freenode.net",
+    nick     => "WebFeat",
+    name     => "A bot for ducks",
+);
+
+my $help = $bot->load('DuckCoHelp');
+
+my $seen = $bot->load('Seen');
+
+my $title = $bot->load('Title');
+
+my $karma = $bot->load('Karma');
+$karma->set('user_ignore_selfkarma' => 0);
+
+$bot->run();
+
